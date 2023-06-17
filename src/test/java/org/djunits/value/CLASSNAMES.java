32a13e0eb75bad7e6bf27051cd2eae9359eecb41
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-06-18 00:39:03 +0200

    Issue #9. Unit Tests updated

--- src/test/java/org/djunits/value/CLASSNAMES.java
+++ src/test/java/org/djunits/value/CLASSNAMES.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.djunits.value.vdouble.matrix.base.AbstractDoubleMatrix;
+import org.djunits.value.vdouble.scalar.base.AbstractDoubleScalar;
+import org.djunits.value.vdouble.vector.base.AbstractDoubleVector;
+import org.djunits.value.vfloat.matrix.base.AbstractFloatMatrix;
+import org.djunits.value.vfloat.scalar.base.AbstractFloatScalar;
+import org.djunits.value.vfloat.vector.base.AbstractFloatVector;
+
 /**
  * CLASSNAMES.java.
  * <p>
@@ -16,35 +23,35 @@ import java.util.List;
 public final class CLASSNAMES
 {
     /** The classes that are absolute (name = class name). */
-    private static final String[] ABS = new String[] { "AbsoluteTemperature", "Direction", "Position", "Time" };
+    private static final String[] ABS = new String[] {"AbsoluteTemperature", "Direction", "Position", "Time"};
 
-    /** List of absolute classes. */
+    /** List of absolute class names. */
     public static final List<String> ABS_LIST;
 
     /** The relative classes that mirror the absolute ones (name = class name). */
-    private static final String[] REL_WITH_ABS = new String[] { "Temperature", "Angle", "Length", "Duration" };
+    private static final String[] REL_WITH_ABS = new String[] {"Temperature", "Angle", "Length", "Duration"};
 
-    /** List of relative-with-absolute classes. */
+    /** List of relative-with-absolute class names. */
     public static final List<String> REL_WITH_ABS_LIST;
 
     /** The classes that are just relative (name = class name). */
-    private static final String[] REL = new String[] { "AbsorbedDose", "Acceleration", "AngularAcceleration", "AngularVelocity",
+    private static final String[] REL = new String[] {"AbsorbedDose", "Acceleration", "AngularAcceleration", "AngularVelocity",
             "AmountOfSubstance", "SolidAngle", "Area", "CatalyticActivity", "Density", "Dimensionless", "ElectricalCapacitance",
             "ElectricalCharge", "ElectricalConductance", "ElectricalCurrent", "ElectricalInductance", "ElectricalPotential",
             "ElectricalResistance", "Energy", "EquivalentDose", "FlowMass", "FlowVolume", "Force", "Frequency", "Illuminance",
             "LinearDensity", "LuminousFlux", "LuminousIntensity", "MagneticFluxDensity", "MagneticFlux", "Mass", "Momentum",
-            "Power", "Pressure", "RadioActivity", "Speed", "Torque", "Volume" };
+            "Power", "Pressure", "RadioActivity", "Speed", "Torque", "Volume"};
 
     /** List of relative classes, except SI. */
     public static final List<String> REL_LIST;
 
-    /** List of all relative classes, except SI. */
+    /** List of all relative class names, except SI. */
     public static final List<String> REL_ALL_LIST;
 
-    /** List of all classes, except SI. */
+    /** List of all class names, except SI. */
     public static final List<String> ALL_LIST;
 
-    /** List of all classes, except Dimensionless and SI. */
+    /** List of all class names, except Dimensionless and SI. */
     public static final List<String> ALL_NODIM_LIST;
 
     static
@@ -65,7 +72,125 @@ public final class CLASSNAMES
     /** */
     private CLASSNAMES()
     {
-        // Utility class
+        // utility class
+    }
+
+    /**
+     * Return the double scalar classes for the quantity name
+     * @param qName quantity name
+     * @return double scalar class for the quantity name
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends AbstractDoubleScalar<?, ?>> doubleScalarClass(final String qName)
+    {
+        try
+        {
+            return (Class<? extends AbstractDoubleScalar<?, ?>>) Class.forName("org.djunits.value.vdouble.scalar." + qName);
+        }
+        catch (ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the float scalar classes for the quantity name
+     * @param qName quantity name
+     * @return list of float scalar classes for the quantity name
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends AbstractFloatScalar<?, ?>> floatScalarClass(final String qName)
+    {
+        try
+        {
+            return (Class<? extends AbstractFloatScalar<?, ?>>) Class.forName("org.djunits.value.vfloat.scalar.Float" + qName);
+        }
+        catch (ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the double vector classes for the quantity name
+     * @param qName quantity name
+     * @return list of double vector classes for the quantity name
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends AbstractDoubleVector<?, ?, ?>> doubleVectorClass(final String qName)
+    {
+        try
+        {
+            return (Class<? extends AbstractDoubleVector<?, ?, ?>>) Class
+                    .forName("org.djunits.value.vdouble.vector." + qName + "Vector");
+        }
+        catch (ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the float vector classes for the quantity name
+     * @param qName quantity name
+     * @return list of float vector classes for the quantity name
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends AbstractFloatVector<?, ?, ?>> floatVectorClass(final String qName)
+    {
+        try
+        {
+            return (Class<? extends AbstractFloatVector<?, ?, ?>>) Class
+                    .forName("org.djunits.value.vfloat.vector.Float" + qName + "Vector");
+        }
+        catch (ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the double matrix classes for the quantity name
+     * @param qName quantity name
+     * @return list of double matrix classes for the quantity name
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends AbstractDoubleMatrix<?, ?, ?, ?>> doubleMatrixClass(final String qName)
+    {
+        try
+        {
+            return (Class<? extends AbstractDoubleMatrix<?, ?, ?, ?>>) Class
+                    .forName("org.djunits.value.vdouble.matrix." + qName + "Matrix");
+        }
+        catch (ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the float matrix classes for the quantity name
+     * @param qName quantity name
+     * @return list of float matrix classes for the quantity name
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends AbstractFloatMatrix<?, ?, ?, ?>> floatMatrixClass(final String qName)
+    {
+        try
+        {
+            return (Class<? extends AbstractFloatMatrix<?, ?, ?, ?>>) Class
+                    .forName("org.djunits.value.vfloat.matrix.Float" + qName + "Matrix");
+        }
+        catch (ClassNotFoundException exception)
+        {
+            exception.printStackTrace();
+        }
+        return null;
     }
 
 }
