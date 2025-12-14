372e4392b6ca6280c9796c951d3a7f14af548bdf
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-12-14 20:22:58 +0100

    Issue #55. Make new quantity package

---
+++ src/main/java/org/djunits/quantity/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The quantity package stores information about quantities with their units.<br>
+ * <br>
+ * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank">https://djutils.org</a>. The DJUTILS project is
+ * distributed under a <a href="https://djutils.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
+ * @author Alexander Verbraeck
+ */
+
+package org.djunits.quantity;
