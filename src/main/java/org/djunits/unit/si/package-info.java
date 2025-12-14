a93a2a3a646d2111331813c2e1d91fc7a8805593
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-12-14 23:54:13 +0100

    Issue #50. Recreate and update the unit.si package

---
+++ src/main/java/org/djunits/unit/si/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The org.djunits.si package contains classes that can work with and manipulate SI prefixes and SI dimensions.<br>
+ * <br>
+ * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank">https://djutils.org</a>. The DJUTILS project is
+ * distributed under a <a href="https://djutils.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
+ * @author Alexander Verbraeck
+ */
+
+package org.djunits.unit.si;
