576efebe7eff41ba4332660edcb902abe45820cc
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-12-21 01:00:09 +0100

    Issue #50. Update the SIDimensions test

--- src/test/java/org/djunits/unit/si/SIDimensionsTest.java
+++ src/test/java/org/djunits/unit/si/SIDimensionsTest.java
@@ -27,10 +27,10 @@ public class SIDimensionsTest
     public void testSIDimensions() throws UnitException
     {
         // all kgm/s2
-        SIDimensions si1 = new SIDimensions(0, 0, 1, 1, -2, 0, 0, 0, 0);
-        SIDimensions si2 = new SIDimensions(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0});
-        SIDimensions si3 = SIDimensions.of("kgm/s2");
-        SIDimensions si4 = SIDimensions.of("kgms-2");
+        SIUnit si1 = new SIUnit(0, 0, 1, 1, -2, 0, 0, 0, 0);
+        SIUnit si2 = new SIUnit(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0});
+        SIUnit si3 = SIUnit.of("kgm/s2");
+        SIUnit si4 = SIUnit.of("kgms-2");
         assertEquals(si1, si2, si1 + "!= " + si2);
         assertEquals(si2, si3, si2 + "!= " + si3);
         assertEquals(si3, si4, si3 + "!= " + si4);
@@ -38,7 +38,7 @@ public class SIDimensionsTest
         assertTrue(si1.equals(si2));
         assertTrue(si1.equals(si1));
         assertFalse(si1.equals(new Object()));
-        assertFalse(si1.equals(SIDimensions.of("kgm2/s2")));
+        assertFalse(si1.equals(SIUnit.of("kgm2/s2")));
         assertFalse(si1.equals(null));
         assertEquals("[0, 0, 1, 1, -2, 0, 0, 0, 0]", si1.toString());
 
@@ -54,41 +54,41 @@ public class SIDimensionsTest
         illegal("/");
 
         // legal "of" instances:
-        SIDimensions.of("rad/s2");
-        SIDimensions.of("rad1/s2");
-        SIDimensions.of("1/s");
-        SIDimensions.of("/s");
-        SIDimensions.of("1/1");
-        SIDimensions.of("1");
-        SIDimensions.of("/1");
-        SIDimensions.of("");
+        SIUnit.of("rad/s2");
+        SIUnit.of("rad1/s2");
+        SIUnit.of("1/s");
+        SIUnit.of("/s");
+        SIUnit.of("1/1");
+        SIUnit.of("1");
+        SIUnit.of("/1");
+        SIUnit.of("");
 
         // check that SIDimensions for Dimensionless are okay
-        assertEquals(SIDimensions.DIMLESS, SIDimensions.of("1/1"), "1/1 == DIMLESS");
-        assertEquals(SIDimensions.DIMLESS, SIDimensions.of("1"), "1 == DIMLESS");
-        assertEquals(SIDimensions.DIMLESS, SIDimensions.of("/1"), "/1 == DIMLESS");
-        assertEquals(SIDimensions.DIMLESS, SIDimensions.of(""), "'' == DIMLESS");
+        assertEquals(SIUnit.DIMLESS, SIUnit.of("1/1"), "1/1 == DIMLESS");
+        assertEquals(SIUnit.DIMLESS, SIUnit.of("1"), "1 == DIMLESS");
+        assertEquals(SIUnit.DIMLESS, SIUnit.of("/1"), "/1 == DIMLESS");
+        assertEquals(SIUnit.DIMLESS, SIUnit.of(""), "'' == DIMLESS");
 
-        SIDimensions si5 = SIDimensions.add(si1, si2);
+        SIUnit si5 = SIUnit.add(si1, si2);
         assertEquals("[0, 0, 2, 2, -4, 0, 0, 0, 0]", si5.toString());
-        SIDimensions si6 = SIDimensions.subtract(si1, si2);
+        SIUnit si6 = SIUnit.subtract(si1, si2);
         assertEquals("[0, 0, 0, 0, 0, 0, 0, 0, 0]", si6.toString());
-        SIDimensions si7 = si1.plus(si2);
+        SIUnit si7 = si1.plus(si2);
         assertEquals("[0, 0, 2, 2, -4, 0, 0, 0, 0]", si7.toString());
-        SIDimensions si8 = si1.minus(si2);
+        SIUnit si8 = si1.minus(si2);
         assertEquals("[0, 0, 0, 0, 0, 0, 0, 0, 0]", si8.toString());
 
-        SIDimensions si9 = SIDimensions.of("rad1sr2kg3m4s5A6K7mol8cd9");
+        SIUnit si9 = SIUnit.of("rad1sr2kg3m4s5A6K7mol8cd9");
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9]", si9.toString());
-        SIDimensions si9a = SIDimensions.of("mol8cd9rad1sr2kg3m4s5A6K7");
+        SIUnit si9a = SIUnit.of("mol8cd9rad1sr2kg3m4s5A6K7");
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9]", si9a.toString());
-        SIDimensions si9b = SIDimensions.of("cd9mol8K7A6s5m4kg3sr2rad1");
+        SIUnit si9b = SIUnit.of("cd9mol8K7A6s5m4kg3sr2rad1");
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9]", si9b.toString());
 
-        SIDimensions siInv = si1.invert();
+        SIUnit siInv = si1.invert();
         assertEquals("[0, 0, -1, -1, 2, 0, 0, 0, 0]", siInv.toString());
 
-        SIDimensions si10 = SIDimensions.of("kgm2s2/kgm2s");
+        SIUnit si10 = SIUnit.of("kgm2s2/kgm2s");
         assertEquals("[0, 0, 0, 0, 1, 0, 0, 0, 0]", si10.toString());
 
         // .
@@ -111,7 +111,7 @@ public class SIDimensionsTest
         assertEquals("kgms<sup>-2</sup>", si1.toHTMLString(false, false));
         assertEquals("kg.m.s<sup>-2</sup>", si1.toHTMLString(false, true));
 
-        SIDimensions si11 = SIDimensions.of("s-3");
+        SIUnit si11 = SIUnit.of("s-3");
         assertEquals("1/s3", si11.toString(true, false));
         assertEquals("1/s3", si11.toString(true, true));
         assertEquals("s-3", si11.toString(false, false));
@@ -131,7 +131,7 @@ public class SIDimensionsTest
         assertEquals("s<sup>-3</sup>", si11.toHTMLString(false, false));
         assertEquals("s<sup>-3</sup>", si11.toHTMLString(false, true));
 
-        SIDimensions si12 = SIDimensions.of("kgm2/s3A");
+        SIUnit si12 = SIUnit.of("kgm2/s3A");
         assertEquals("kgm2/s3A", si12.toString(true, false));
         assertEquals("kg.m2/s3.A", si12.toString(true, true));
         assertEquals("kgm2s-3A-1", si12.toString(false, false));
@@ -152,7 +152,7 @@ public class SIDimensionsTest
         assertEquals("kg.m<sup>2</sup>.s<sup>-3</sup>.A<sup>-1</sup>", si12.toHTMLString(false, true));
 
         // fractional
-        SIDimensions sif = new SIDimensions(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0}, new byte[] {1, 1, 1, 1, 1, 1, 1, 1, 1});
+        SIUnit sif = new SIUnit(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0}, new byte[] {1, 1, 1, 1, 1, 1, 1, 1, 1});
         assertEquals(sif, si1);
         assertEquals(sif, si2);
         assertEquals(sif, si3);
@@ -163,14 +163,14 @@ public class SIDimensionsTest
         assertFalse(sif.isFractional()); // unit denominator
         assertFalse(si2.isFractional());
 
-        SIDimensions sif2 = new SIDimensions(new byte[] {0, 0, 1, 1, -2, -1, 0, 0, 0}, new byte[] {1, 1, 2, 2, 1, 3, 1, 1, 1});
+        SIUnit sif2 = new SIUnit(new byte[] {0, 0, 1, 1, -2, -1, 0, 0, 0}, new byte[] {1, 1, 2, 2, 1, 3, 1, 1, 1});
         assertNotEquals(sif, sif2);
         assertNotEquals(sif.hashCode(), sif2.hashCode());
         assertEquals("[0, 0, 1/2, 1/2, -2, -1/3, 0, 0, 0]", sif2.toString());
 
         try
         {
-            SIDimensions siw1 = new SIDimensions(new byte[] {0, 0, 1, 1, -2, 0, 0, 0}); // 8 long
+            SIUnit siw1 = new SIUnit(new byte[] {0, 0, 1, 1, -2, 0, 0, 0}); // 8 long
             fail("SIDimensions.of(" + siw1 + ") should have triggered a UnitException");
         }
         catch (SIRuntimeException e)
@@ -180,7 +180,7 @@ public class SIDimensionsTest
 
         try
         {
-            SIDimensions siw2 = new SIDimensions(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0, 0}); // 6 long
+            SIUnit siw2 = new SIUnit(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0, 0}); // 6 long
             fail("SIDimensions.of(" + siw2 + ") should have triggered a UnitException");
         }
         catch (SIRuntimeException e)
@@ -190,7 +190,7 @@ public class SIDimensionsTest
 
         try
         {
-            SIDimensions siw3 = new SIDimensions(new byte[] {0, 0, 1, 1, -2, 0, 0, 0}, new byte[] {1, 1, 2, 2, 1, 3, 1, 1, 1});
+            SIUnit siw3 = new SIUnit(new byte[] {0, 0, 1, 1, -2, 0, 0, 0}, new byte[] {1, 1, 2, 2, 1, 3, 1, 1, 1});
             fail("SIDimensions.of(" + siw3 + ") should have triggered a UnitException");
         }
         catch (SIRuntimeException e)
@@ -200,8 +200,8 @@ public class SIDimensionsTest
 
         try
         {
-            SIDimensions siw4 =
-                    new SIDimensions(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0}, new byte[] {1, 1, 2, 2, 1, 3, 1, 1, 1, 1});
+            SIUnit siw4 =
+                    new SIUnit(new byte[] {0, 0, 1, 1, -2, 0, 0, 0, 0}, new byte[] {1, 1, 2, 2, 1, 3, 1, 1, 1, 1});
             fail("SIDimensions.of(" + siw4 + ") should have triggered a UnitException");
         }
         catch (SIRuntimeException e)
@@ -217,7 +217,7 @@ public class SIDimensionsTest
     {
         try
         {
-            SIDimensions.of(si);
+            SIUnit.of(si);
             fail("SIDimensions.of(" + si + ") should have triggered a UnitException");
         }
         catch (UnitException e)
