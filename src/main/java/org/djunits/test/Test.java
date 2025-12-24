d5444ead78ce4e4273811969eca19a71ae454526
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-12-25 00:03:12 +0100

    Issue #50. Test duration units

--- src/main/java/org/djunits/test/Test.java
+++ src/main/java/org/djunits/test/Test.java
@@ -1,6 +1,7 @@
 package org.djunits.test;
 
 import org.djunits.quantity.AbsoluteTemperature;
+import org.djunits.quantity.Duration;
 import org.djunits.quantity.Length;
 import org.djunits.unit.Units;
 
@@ -17,39 +18,55 @@ public final class Test
     /** */
     private Test()
     {
-        // length();
+        length();
         temp();
+        duration();
     }
 
     /** */
     private void length()
     {
+        System.out.println("\nLENGTH");
         Length length = new Length(5.0, Units.m);
         System.out.println(length);
 
         Length l2 = new Length(5.0, Units.km);
         System.out.println(l2.toDisplayString());
         System.out.println("SI = " + l2.si() + " [" + l2.getDisplayUnit().getBaseUnit().getId() + "]");
-        
+
         Length l3 = new Length(10.0, "Qm");
         System.out.println(l3);
         System.out.println(l3.si());
-        
+
         Length l4 = new Length(Math.PI, "mum");
         System.out.println(l4);
         System.out.println(l4.plus(length));
         System.out.println(length.plus(l4));
     }
-    
+
     /** */
     private void temp()
     {
+        System.out.println("\nTEMPERATURE");
         System.out.println("Temperature");
         var t1 = AbsoluteTemperature.valueOf("10 degC");
         System.out.println(t1 + "  si=" + t1.si());
         System.out.println(AbsoluteTemperature.of(10, "K"));
     }
-    
+
+    /** */
+    private void duration()
+    {
+        System.out.println("\nDURATION");
+        var d1 = Duration.of(42.0, "ms");
+        System.out.println("42 ms: " + d1);
+        var f1 = d1.reciprocal();
+        System.out.println("reciprocal: " + f1);
+        var d2 = Duration.valueOf("1 day");
+        // System.out.println("1 day: " + d2 + " = " + d2.setDisplayUnit("s") + " seconds");
+        System.out.println("1 day: " + d2 + " = " + d2.setDisplayUnit(Duration.Unit.SECOND));
+    }
+
     /**
      * @param args not used
      */
