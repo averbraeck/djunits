179d0329157ba3cf6346b2a15180ea1ff66a8b33
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-12-30 18:24:54 +0100

    Issue #67. Expand the Normed interface for vectors

--- src/main/java/org/djunits/vecmat/Normed.java
+++ src/main/java/org/djunits/vecmat/Normed.java
@@ -16,8 +16,37 @@ import org.djunits.unit.UnitInterface;
 public interface Normed<Q extends Quantity<Q, U>, U extends UnitInterface<U, Q>>
 {
     /**
-     * Return the norm of this element, expressed as a quantity.
+     * Return the norm of this element, expressed as a quantity. The standard norm is the L2-norm.
      * @return the norm of this element, expressed as a quantity
      */
-    Q norm();
+    default Q norm()
+    {
+        return normL2();
+    }
+
+    /**
+     * Return the L1-norm of this element, expressed as a quantity. The L1-norm is |x1| + |x2| + ... + |xn|.
+     * @return the L1-norm of this element, expressed as a quantity
+     */
+    Q normL1();
+
+    /**
+     * Return the L2-norm of this element, expressed as a quantity. The L2-norm is sqrt(x1^2 + x2^2 + ... + xn^2).
+     * @return the L2-norm of this element, expressed as a quantity
+     */
+    Q normL2();
+
+    /**
+     * Return the Lp-norm of this element, expressed as a quantity. The Ln-norm is (x1^p + x2^p + ... + xn^p)^(1/p).
+     * @param p the rank of the norm
+     * @return the Lp-norm of this element, expressed as a quantity
+     */
+    Q normLp(int p);
+
+    /**
+     * Return the L&infin;-norm of this element, expressed as a quantity. The L&infin;-norm is max(|x1|, |x2|, ..., |xn|).
+     * @return the L&infin;-norm of this element, expressed as a quantity
+     */
+    Q normLinf();
+
 }
