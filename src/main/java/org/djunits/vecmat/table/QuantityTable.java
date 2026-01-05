70ebf76fa7a91b88ed49de35106385be30d51b0e
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-01-05 15:22:42 +0100

    Issue #75. Add QuantityTable

---
+++ src/main/java/org/djunits/vecmat/table/QuantityTable.java
@@ -0,0 +1,106 @@
+package org.djunits.vecmat.table;
+
+import org.djunits.quantity.SIQuantity;
+import org.djunits.quantity.def.Quantity;
+import org.djunits.unit.UnitInterface;
+import org.djunits.unit.si.SIUnit;
+import org.djunits.util.ArrayMath;
+import org.djunits.vecmat.Matrix;
+import org.djunits.vecmat.dnxm.MatrixNxM;
+import org.djunits.vecmat.operations.Hadamard;
+import org.djunits.vecmat.storage.DataGrid;
+import org.djutils.exceptions.Throw;
+
+/**
+ * QuantityTable is a two-dimensonal table with quantities. The QuantityTable allows for Hadamard (element-wise) operations, but
+ * not for vector/matrix operations. A QuantityTable can be transformed to a MatrixNxM or vice versa. <br>
+ * <br>
+ * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank">https://djutils.org</a>. The DJUTILS project is
+ * distributed under a <a href="https://djutils.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
+ * @author Alexander Verbraeck
+ * @param <Q> the quantity type
+ * @param <U> the unit type
+ */
+public class QuantityTable<Q extends Quantity<Q, U>, U extends UnitInterface<U, Q>> extends Matrix<Q, U, QuantityTable<Q, U>>
+        implements Hadamard<QuantityTable<?, ?>>
+{
+    /** */
+    private static final long serialVersionUID = 600L;
+
+    /** The data of the matrix, in SI unit. */
+    private final DataGrid<?> dataSi;
+
+    /**
+     * Create a new NxM Matrix with a unit, based on a DataGrid storage object that contains SI data.
+     * @param dataSi the data of the matrix, in SI unit.
+     * @param displayUnit the display unit to use
+     */
+    public QuantityTable(final DataGrid<?> dataSi, final U displayUnit)
+    {
+        super(displayUnit, dataSi.rows(), dataSi.cols());
+        Throw.whenNull(dataSi, "dataSi");
+        this.dataSi = dataSi;
+    }
+
+    @Override
+    public QuantityTable<Q, U> instantiate(final double[] siNew)
+    {
+        return new QuantityTable<Q, U>(this.dataSi.instantiate(siNew), getDisplayUnit());
+    }
+
+    @Override
+    public double[] si()
+    {
+        return this.dataSi.getDataArray();
+    }
+
+    @Override
+    public double si(final int row, final int col)
+    {
+        // internal storage is 0-based, user access is 1-based
+        return this.dataSi.get(row - 1, col - 1);
+    }
+
+    @Override
+    public QuantityTable<?, ?> invertElements()
+    {
+        SIUnit siUnit = getDisplayUnit().siUnit().invert();
+        return new QuantityTable<SIQuantity, SIUnit>(this.dataSi.instantiate(ArrayMath.reciprocal(si())), siUnit);
+    }
+
+    @Override
+    public QuantityTable<?, ?> multiplyElements(final QuantityTable<?, ?> other)
+    {
+        SIUnit siUnit = SIUnit.add(getDisplayUnit().siUnit(), other.getDisplayUnit().siUnit());
+        return new QuantityTable<SIQuantity, SIUnit>(this.dataSi.instantiate(ArrayMath.multiply(si(), other.si())), siUnit);
+    }
+
+    @Override
+    public QuantityTable<?, ?> divideElements(final QuantityTable<?, ?> other)
+    {
+        SIUnit siUnit = SIUnit.subtract(getDisplayUnit().siUnit(), other.getDisplayUnit().siUnit());
+        return new QuantityTable<SIQuantity, SIUnit>(this.dataSi.instantiate(ArrayMath.divide(si(), other.si())), siUnit);
+    }
+
+    /**
+     * Return the matrix 'as' a matrix with a known quantity, using a unit to express the result in. Throw a Runtime exception
+     * when the SI units of this vector and the target vector do not match.
+     * @param targetUnit the unit to convert the matrix to
+     * @return a matrix typed in the target matrix class
+     * @throws IllegalArgumentException when the units do not match
+     * @param <TQ> target quantity type
+     * @param <TU> target unit type
+     */
+    public <TQ extends Quantity<TQ, TU>, TU extends UnitInterface<TU, TQ>> QuantityTable<TQ, TU> as(final TU targetUnit)
+            throws IllegalArgumentException
+    {
+        Throw.when(!getDisplayUnit().siUnit().equals(targetUnit.siUnit()), IllegalArgumentException.class,
+                "QuantityTable.as(%s) called, but units do not match: %s <> %s", targetUnit,
+                getDisplayUnit().siUnit().getDisplayAbbreviation(), targetUnit.siUnit().getDisplayAbbreviation());
+        return new QuantityTable<TQ, TU>(this.dataSi.instantiate(si()), targetUnit);
+    }
+
+    // TODO add methods asMatrix2x2, asMatrix3x3, asMatrixNxN, asMatrixNxM
+
+}
---
+++ src/main/java/org/djunits/vecmat/table/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Two-dimensonal tables with quantities. The tables allow for Hadamard (element-wise) operations, but not for vector/matrix
+ * operations. A Table can be transformed to a MatrixNxM or vice versa.<br>
+ * <br>
+ * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank">https://djutils.org</a>. The DJUTILS project is
+ * distributed under a <a href="https://djutils.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
+ * @author Alexander Verbraeck
+ */
+package org.djunits.vecmat.table;
