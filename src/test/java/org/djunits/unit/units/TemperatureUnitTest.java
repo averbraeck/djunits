614e6aee504b6b8b5b48d006d1b7225f643bd542
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-02-28 18:06:53 +0100

    Issue #80. temperature test should use kelvin, not Kelvin

--- src/test/java/org/djunits/unit/units/TemperatureUnitTest.java
+++ src/test/java/org/djunits/unit/units/TemperatureUnitTest.java
@@ -36,7 +36,7 @@ public class TemperatureUnitTest extends AbstractLinearUnitTest<Temperature.Unit
     public final void conversions()
     {
         assertEquals("K", Temperature.Unit.SI_UNIT.toString(true, false));
-        checkUnitRatioNameAndAbbreviation(Temperature.Unit.K, 1.0, 0.00001, "Kelvin", "K");
+        checkUnitRatioNameAndAbbreviation(Temperature.Unit.K, 1.0, 0.00001, "kelvin", "K");
         checkUnitRatioNameAndAbbreviation(Temperature.Unit.degC, 1.0, 0.000001, "degree Celsius", "\u00B0C");
         checkUnitRatioNameAndAbbreviation(Temperature.Unit.degF, 5.0 / 9.0, 0.00001, "degree Fahrenheit", "\u00B0F");
         // Check two conversions between non-standard units
