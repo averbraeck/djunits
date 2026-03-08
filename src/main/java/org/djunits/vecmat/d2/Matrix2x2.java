a5a058c7d2599d49b1c73fcc75c20c9951a3ec54
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-03-08 13:19:19 +0100

    Repair bug is as() method

--- src/main/java/org/djunits/vecmat/d2/Matrix2x2.java
+++ src/main/java/org/djunits/vecmat/d2/Matrix2x2.java
@@ -167,7 +167,7 @@ public class Matrix2x2<Q extends Quantity<Q, U>, U extends UnitInterface<U, Q>> 
         Throw.when(!getDisplayUnit().siUnit().equals(targetUnit.siUnit()), IllegalArgumentException.class,
                 "Matrix2x2.as(%s) called, but units do not match: %s <> %s", targetUnit,
                 getDisplayUnit().siUnit().getDisplayAbbreviation(), targetUnit.siUnit().getDisplayAbbreviation());
-        return new Matrix2x2<TQ, TU>(si(), targetUnit);
+        return new Matrix2x2<TQ, TU>(si(), targetUnit.getBaseUnit()).setDisplayUnit(targetUnit);
     }
 
 }
