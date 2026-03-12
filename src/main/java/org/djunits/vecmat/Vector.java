b8c6d49e09b557f32e2910dc1a0f0777b7c6cf38
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-03-12 15:03:09 +0100

    Issue #99. Turn Vector into an abstract class in the new hierarchy

--- src/main/java/org/djunits/vecmat/Vector.java
+++ src/main/java/org/djunits/vecmat/Vector.java
@@ -1,15 +1,16 @@
-package org.djunits.vecmat.d2;
+package org.djunits.vecmat;
 
 import java.util.Iterator;
 
+import org.djunits.quantity.SIQuantity;
 import org.djunits.quantity.def.Quantity;
 import org.djunits.unit.UnitInterface;
-import org.djunits.vecmat.VectorMatrix;
+import org.djunits.unit.si.SIUnit;
 import org.djunits.vecmat.operations.Normed;
 
 /**
- * VectorOps contains the contract for Vector classes. In addition, Vector classes can implement other interfaces as well, such
- * as VectorMatrixOps and VectorTransposable.
+ * Vector contains the contract for Vector classes. In addition, Vector classes can implement other interfaces as well, such
+ * as VectorTransposable.
  * <p>
  * Copyright (c) 2025-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djunits.org" target="_blank">https://djunits.org</a>. The DJUNITS project is
@@ -17,16 +18,31 @@ import org.djunits.vecmat.operations.Normed;
  * @author Alexander Verbraeck
  * @param <Q> the quantity type
  * @param <U> the unit type
- * @param <V> the vector or matrix type
+ * @param <V> the vector type
+ * @param <SI> the vector type with generics &lt;SIQuantity, SIUnit&lt;
+ * @param <H> the generic vector type with generics &lt;?, ?&lt; for Hadamard operations
  */
-public interface Vector<Q extends Quantity<Q, U>, U extends UnitInterface<U, Q>, V extends Vector<Q, U, V>>
-        extends VectorMatrix<Q, U, V>, Iterable<Q>, Normed<Q, U>
+public abstract class Vector<Q extends Quantity<Q, U>, U extends UnitInterface<U, Q>, V extends VectorMatrix<Q, U, V, SI, H>,
+        SI extends VectorMatrix<SIQuantity, SIUnit, SI, ?, ?>, H extends VectorMatrix<?, ?, ?, ?, ?>>
+        extends VectorMatrix<Q, U, V, SI, H> implements Iterable<Q>, Normed<Q, U>
 {
+    /** */
+    private static final long serialVersionUID = 600L;
+    
+    /**
+     * Create a new Vector with a unit, as an extension of Matrix.
+     * @param displayUnit the display unit to use
+     */
+    public Vector(final U displayUnit)
+    {
+        super(displayUnit);
+    }
+
     /**
      * Retrieve the size of the vector.
      * @return the size of the vector
      */
-    int size();
+    public abstract int size();
 
     /**
      * Retrieve a value from the vector.
@@ -34,19 +50,19 @@ public interface Vector<Q extends Quantity<Q, U>, U extends UnitInterface<U, Q>,
      * @return the value as a Scalar
      * @throws IndexOutOfBoundsException in case index is out of bounds
      */
-    Q get(int index) throws IndexOutOfBoundsException;
+    public abstract Q get(int index) throws IndexOutOfBoundsException;
 
     /**
      * Return the vector as an array of scalars.
      * @return the vector as an array of scalars
      */
-    Q[] getScalarArray();
+    public abstract Q[] getScalarArray();
 
     /**
      * Create and return an iterator over the scalars in this vector in proper sequence.
      * @return an iterator over the scalars in this vector in proper sequence
      */
     @Override
-    Iterator<Q> iterator();
+    public abstract Iterator<Q> iterator();
 
 }
