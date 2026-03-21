3436217a1501e68497b4d99192effb2adabf89be
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-03-21 14:35:08 +0100

    package-info for package vecmat.dn updated

--- src/main/java/org/djunits/vecmat/dn/package-info.java
+++ src/main/java/org/djunits/vecmat/dn/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Square vector and matrix implementations with dimension N.
+ * Square matrix implementations with dimension N.
  * <p>
  * Copyright (c) 2025-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djunits.org" target="_blank">https://djunits.org</a>. The DJUNITS project is
