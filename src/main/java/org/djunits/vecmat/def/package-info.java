56d12e52b09c090397544fe5b25d0125273ca320
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-03-21 12:22:40 +0100

    Update package definition

--- src/main/java/org/djunits/vecmat/def/package-info.java
+++ src/main/java/org/djunits/vecmat/def/package-info.java
@@ -1,7 +1,7 @@
 /**
- * Definition classes for vectors and matrices.<br>
- * <br>
- * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Definition classes for vectors and matrices.
+ * <p>
+ * Copyright (c) 2026-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djutils.org" target="_blank">https://djutils.org</a>. The DJUTILS project is
  * distributed under a <a href="https://djutils.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
  * @author Alexander Verbraeck
