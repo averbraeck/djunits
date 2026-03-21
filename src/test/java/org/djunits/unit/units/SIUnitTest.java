1b2cdf902d95ce94b54bbd032abbceb6e288d6e1
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-03-21 12:26:53 +0100

    Update copyright year

--- src/test/java/org/djunits/unit/units/SIUnitTest.java
+++ src/test/java/org/djunits/unit/units/SIUnitTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * SIUnitTest tests a few remaining methods of the SIUnit class.<p>
- * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2025-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djunits.org" target="_blank">https://djunits.org</a>. The DJUTILS project is
  * distributed under a <a href="https://djunits.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
  * @author Alexander Verbraeck
--- src/test/java/org/djunits/vecmat/storage/DataGridSiTest.java
+++ src/test/java/org/djunits/vecmat/storage/DataGridSiTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 /**
  * DataGridSiTest tests equals, hashCode, equals(obj, eps) for the different DataGridSi classes.<br>
  * <br>
- * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2025-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djunits.org" target="_blank">https://djunits.org</a>. The DJUTILS project is
  * distributed under a <a href="https://djunits.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
  * @author Alexander Verbraeck
