1293f770225b87ed7a631fa637b1067b8baa9cbd
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-04-10 22:03:06 +0200

    Issue #148. Matrix3x3 of(A[]), of(A[][]) added

--- src/main/java/org/djunits/vecmat/d3/AbsMatrix3x3.java
+++ src/main/java/org/djunits/vecmat/d3/AbsMatrix3x3.java
@@ -5,6 +5,7 @@ import org.djunits.quantity.def.Quantity;
 import org.djunits.quantity.def.Reference;
 import org.djunits.unit.Unit;
 import org.djunits.vecmat.def.AbsSquareMatrix;
+import org.djutils.exceptions.Throw;
 
 /**
  * AbsMatrix3x3 implements a matrix with 3x3 absolute quantities with a reference point. The matrix is immutable, except for the
@@ -119,6 +120,32 @@ public class AbsMatrix3x3<A extends AbsQuantity<A, Q, ?>, Q extends Quantity<Q>>
     }
 
     /**
+     * Create an AbsMatrix3x3 without needing generics.
+     * @param absData the {a11, a12, a13, ..., a31, a32, a33} values expressed as an array of absolute quantities
+     * @return a new Matrix3x3 with a unit
+     * @param <A> the absolute quantity type
+     * @param <Q> the quantity type
+     * @param <R> the reference type
+     */
+    public static <A extends AbsQuantity<A, Q, R>, Q extends Quantity<Q>,
+            R extends Reference<R, A, Q>> AbsMatrix3x3<A, Q> of(final A[] absData)
+    {
+        Throw.whenNull(absData, "absData");
+        Throw.when(absData.length != 9, IllegalArgumentException.class, "absData.length != 9");
+        Throw.whenNull(absData[0], "absData[0]");
+        Reference<?, A, Q> reference = absData[0].getReference();
+        double[] dataSi = new double[9];
+        for (int i = 0; i < 9; i++)
+        {
+            Throw.whenNull(absData[i], "absData[%d] = null", i);
+            Throw.when(!reference.equals(absData[i].getReference()), IllegalArgumentException.class,
+                    "absData[%d].reference %s != %s", i, absData[i].getReference().toString(), reference.toString());
+            dataSi[i] = absData[i].si();
+        }
+        return new AbsMatrix3x3<>(Matrix3x3.ofSi(dataSi, absData[0].getDisplayUnit()), absData[0].getReference());
+    }
+
+    /**
      * Create an AbsMatrix3x3 with a unit, based on a 2-dimensional grid with SI-values.
      * @param gridSi the matrix values {{a11, a12, 13}, ..., {a31, a32, a33}} expressed in the SI or base unit
      * @param displayUnit the unit of the data, which will also be used as the display unit
@@ -165,6 +192,40 @@ public class AbsMatrix3x3<A extends AbsQuantity<A, Q, ?>, Q extends Quantity<Q>>
             R extends Reference<R, A, Q>> AbsMatrix3x3<A, Q> of(final Q[][] grid, final R reference)
     {
         return new AbsMatrix3x3<>(Matrix3x3.of(grid), reference);
+    }
+
+    /**
+     * Create an AbsMatrix3x3 without needing generics.
+     * @param absGrid the {{a11, a12, a13}, ..., {a31, a32, a33}} values expressed as an grid [][] of absolute quantities
+     * @return a new Matrix3x3 with a unit
+     * @param <A> the absolute quantity type
+     * @param <Q> the quantity type
+     * @param <R> the reference type
+     */
+    public static <A extends AbsQuantity<A, Q, R>, Q extends Quantity<Q>,
+            R extends Reference<R, A, Q>> AbsMatrix3x3<A, Q> of(final A[][] absGrid)
+    {
+        Throw.whenNull(absGrid, "absData");
+        Throw.when(absGrid.length != 3, IllegalArgumentException.class, "absData.length != 3");
+        Throw.whenNull(absGrid[0], "absData[0]");
+        Throw.when(absGrid[0].length != 3, IllegalArgumentException.class, "absData[0].length != 3");
+        Throw.whenNull(absGrid[0][0], "absData[0][0]");
+        Reference<?, A, Q> reference = absGrid[0][0].getReference();
+        double[] dataSi = new double[9];
+        for (int r = 0; r < 3; r++)
+        {
+            Throw.whenNull(absGrid[r], "absData[%d] = null", r);
+            Throw.when(absGrid[r].length != 3, IllegalArgumentException.class, "absData[%d].length != 3", r);
+            for (int c = 0; c < 3; c++)
+            {
+                Throw.whenNull(absGrid[r][c], "absData[%d][%d] = null", r, c);
+                Throw.when(!reference.equals(absGrid[r][c].getReference()), IllegalArgumentException.class,
+                        "absData[%d][%d].reference %s != %s", r, c, absGrid[r][c].getReference().toString(),
+                        reference.toString());
+                dataSi[r * 3 + c] = absGrid[r][c].si();
+            }
+        }
+        return new AbsMatrix3x3<>(Matrix3x3.ofSi(dataSi, absGrid[0][0].getDisplayUnit()), reference);
     }
 
     /**
