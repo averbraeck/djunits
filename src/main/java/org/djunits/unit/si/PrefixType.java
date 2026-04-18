ba40a979a962698d924172f32ccfe88b769c4226
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-04-18 20:22:01 +0200

    Issue #152. Add a PrefixType (UNIT, PER_UNIT, KILO, etc.)

---
+++ src/main/java/org/djunits/unit/si/PrefixType.java
@@ -0,0 +1,27 @@
+package org.djunits.unit.si;
+
+/**
+ * PrefixType describes the type of prefix, e.g., KILO, or PER_KILO.
+ * <p>
+ * Copyright (c) 2026-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank">https://djutils.org</a>. The DJUTILS project is
+ * distributed under a <a href="https://djutils.org/docs/license.html" target="_blank">three-clause BSD-style license</a>.
+ * @author Alexander Verbraeck
+ */
+public enum PrefixType
+{
+    /** Unit. */
+    UNIT,
+    
+    /** Per unit. */
+    PER_UNIT,
+    
+    /** Kilo. */
+    KILO,
+    
+    /** Per kilo. */
+    PER_KILO,
+    
+    /** Positive unit. */
+    POS_UNIT;
+}
