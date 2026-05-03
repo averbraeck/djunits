493565f9a39815414e5b623428e20b76d380cdea
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-03 18:04:30 +0200

    Issue #152. Update package-info.java

--- src/main/java/org/djunits/formatter/package-info.java
+++ src/main/java/org/djunits/formatter/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Methods to format values for display.
+ * Methods to format quantity-related values for display.
  * <p>
  * Copyright (c) 2025-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djunits.org" target="_blank">https://djunits.org</a>. The DJUNITS project is
