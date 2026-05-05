95156695d52225206a09efe5c4771d47225a3242
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-05 13:55:14 +0200

    Issue #105. Rename scale method to use Identity instead of Base

--- src/main/java/org/djunits/unit/scale/Scale.java
+++ src/main/java/org/djunits/unit/scale/Scale.java
@@ -3,8 +3,8 @@ package org.djunits.unit.scale;
 import java.io.Serializable;
 
 /**
- * Scales for unit conversion, offers functions to and from SI units. E.g., LinearScale for Length, Area, etc. LinearOffsetScale
- * for Temperature. PercentScale for Angle. LogarithmicScale for Sound, GradeScale for percentual angle.
+ * Scales for unit conversion, offers functions to and from SI units. E.g., {@link LinearScale} for Length, Area, etc.
+ * {@link GradeScale} for percentual angle.
  * <p>
  * Copyright (c) 2013-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
@@ -14,24 +14,24 @@ import java.io.Serializable;
 public interface Scale extends Serializable
 {
     /**
-     * Convert a value expressed in this unit to its base (SI) value.
+     * Convert a value expressed in this unit to its base (SI) value on the identity scale.
      * @param value the value expressed in this unit
      * @return the value converted to its SI value
      */
-    double toBaseValue(double value);
+    double toIdentityScale(double value);
 
     /**
-     * Convert a value from a base (SI) value to a value in the unit that uses this scale.
+     * Convert a value from a base (SI) value on the identity scale to a value in the unit that uses this scale.
      * @param value the value to convert
      * @return the corresponding value in the given unit
      */
-    double fromBaseValue(double value);
+    double fromIdentityScale(double value);
 
     /**
-     * Return whether a scale is a 'standard' scale. For a linear scale, any scale with conversion factor 1 would be considered
-     * standard. For an offset scale, it would be considered standard if the offset is 0 and the conversion factor is 1.
-     * @return whether the scale is a 'standard' scale
+     * Return whether a scale is an 'identity' scale. For a linear scale, any scale with conversion factor 1 would be considered
+     * an identity scale.
+     * @return whether the scale is an 'identity' scale
      */
-    boolean isBaseScale();
+    boolean isIdentityScale();
 
 }
